ed77cb7f266797381c1abf25b1a0168b41da4bb3
Charlie Truong <charlie.m.truong@gmail.com>
2015-02-22 08:50:54 -0600

    scrollHide directive translates element on y-axis upwards by window scroll down amount.

--- src/app/main/scroll-hide-spec.js
+++ src/app/main/scroll-hide-spec.js
@@ -1,18 +1,31 @@
 'use strict';
 
 describe('controllers', function(){
-  var $scope;
+  var $scrollHideEl, $$window;
 
   beforeEach(module('scrollHide'));
 
-  beforeEach(inject(function($rootScope, $compile) {
-    $scope = $rootScope.$new();
-    var container = document.createElement('div');
-    container.innerHTML = '<div scroll-hide="50"></div>';
-    $compile(container)($scope);
-  }));
+  beforeEach(function() {
+    var $window = document.createElement('div');
+    $$window = angular.element($window);
 
-  it('should return the passed parameter', function() {
-    expect($scope.param).toEqual(50);
+    module(function($provide) {
+      $provide.value('$window', $window);
+    });
+
+    inject(function($rootScope, $compile) {
+      var $scope = $rootScope.$new();
+      var container = document.createElement('div');
+      container.innerHTML = '<div scroll-hide="50"></div>';
+      $scrollHideEl = $compile(container)($scope).find('div');
+    })
+  });
+
+  describe('on window scroll', function() {
+    it('should transform the y-position by the scroll amount', function() {
+      $$window[0].pageYOffset = 5;
+      $$window.triggerHandler('scroll');
+      expect($scrollHideEl.css('transform')).toEqual('translateY(-5px)');
+    });
   });
 });
--- src/app/main/scroll-hide.js
+++ src/app/main/scroll-hide.js
@@ -1,10 +1,19 @@
 'use strict';
 
 angular.module('scrollHide')
-.directive('scrollHide', function () {
+.directive('scrollHide', function($window) {
   return {
-    link: function($scope, element, attrs) {
-      $scope.param = parseInt(attrs.scrollHide, 10);
+    link: function($scope, $element, attrs) {
+      var scrollLimit = parseInt(attrs.scrollHide, 10);
+      var $$window = angular.element($window);
+      var translateY = function(pixels) {
+        return 'translateY(' + pixels + 'px)';
+      };
+
+      $$window.bind('scroll', function() {
+        var pixelsToScroll = $window.pageYOffset * -1;
+        $element.css({transform: translateY(pixelsToScroll)});
+      });
     }
   }
 });
